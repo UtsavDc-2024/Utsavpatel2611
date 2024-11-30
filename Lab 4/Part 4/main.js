e8b6ace2387b44e6501cc5dd8ab4435d6c550752
Utsav-Patel-77 <utsavmukeshbhai.patel@dcmail.ca>
2024-11-29 22:42:09 -0500

    Update main.js

--- Lab 4/Part 4/main.js
+++ Lab 4/Part 4/main.js
@@ -4,12 +4,12 @@ const ctx = canvas.getContext('2d');
 const startButton = document.getElementById('startButton');
 // Ball properties
 let ball = {
-    x: 50, // Initial x position
+    x: 48, // Initial x position
     y: 50, // Initial y position
     radius: 20, // Ball radius
     speedX: 4, // Speed of ball in X direction
     speedY: 4, // Speed of ball in Y direction
-    color: 'red', // Ball color
+    color: 'red' , // Ball color
   };
   // Draw the ball on the canvas
 function drawBall() {
